40012d29e906dd469998ea7d85f974e5bff23c56
Victor Jansen <victorfjansen@gmail.com>
2021-05-09 18:36:31 -0300

    Tooltip refatorada

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,42 +1,64 @@
-export default function initToolTip(){
-   const tooltips = document.querySelectorAll('[data-tooltip]')
+export default class ToolTip{
+  constructor(tooltips){
+    this.tooltips = document.querySelectorAll(tooltips)
 
-   const onMouseMove = {
-    handleEvent(event){
+    // bind of objects of classes to callbacks
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+    this.onMouseMove = this.onMouseMove.bind(this)
+    this.onMouseOver = this.onMouseOver.bind(this)
+
+  }
+  
+  // Move the tooltip getting reference of your styles
+  // according your mouse positions
+  onMouseMove(event){
       this.tooltipBox.style.top = `${event.pageY + 20}px`
-      this.tooltipBox.style.left = `${event.pageX + 20}px`
+      if(event.pageX + 240 > window.innerWidth){
+        this.tooltipBox.style.left = `${event.pageX - 190}px`
+      }else{
+        this.tooltipBox.style.left = `${event.pageX + 20}px`
+      }
     }
-  }
+  
 
-  const onMouseLeave = {
-    handleEvent(){
-      this.tooltipBox.remove()      
-    }
+  // remove the tooltip and your events ( mouseMove and mouseLeave)
+  onMouseLeave({currentTarget}){
+      this.tooltipBox.remove()     
+      currentTarget.removeEventListener("mouseleave", this.onMouseLeave) 
+      currentTarget.removeEventListener("mousemove", this.onMouseMove) 
   }
-
-   function criarTooltipBox(element){
+  
+  // creating tooltip box and put in body
+  criarTooltipBox(element){
     const tooltipBox = document.createElement("div")
     const text = element.getAttribute("aria-label")
     tooltipBox.classList.add("tooltip")
     tooltipBox.innerText = text
     document.body.appendChild(tooltipBox)
-    return tooltipBox
+    this.tooltipBox = tooltipBox
   }
 
-   function onMouseOver(){
-    const tooltipBox = criarTooltipBox(this)
-    
-    onMouseMove.tooltipBox = tooltipBox
-    this.addEventListener("mousemove", onMouseMove)
-
-    onMouseLeave.tooltipBox = tooltipBox
-    this.addEventListener("mouseleave", onMouseLeave)
-    
+  // create the tooltip and add the respective events to target
+  onMouseOver({ currentTarget }){
+    // creating the tooltipbox and put one eventListener
+    this.criarTooltipBox(currentTarget) 
+    currentTarget.addEventListener("mousemove", this.onMouseMove)
+    currentTarget.addEventListener("mouseleave", this.onMouseLeave) 
   }
 
-  tooltips.forEach((item) =>{
-    item.addEventListener('mouseover', onMouseOver)
-  })   
+  // add the events on each item of "this.tooltip"
+  addTooltipsEvent(){
+    this.tooltips.forEach((item) =>{
+      item.addEventListener('mouseover', this.onMouseOver)
+    }) 
+  } 
+  
+  init(){
+    if( this.tooltips.length){
+      this.addTooltipsEvent()
+    }
+    return this
+  }
 }
 
 
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from "./modules/scroll-suave.js"
 import Accordion from "./modules/animacao-accordion.js"
 import TabNav from "./modules/init-tab-nav.js"
 import Modal from "./modules/modal.js"
-import initToolTip from "./modules/tooltip.js"
+import ToolTip from "./modules/tooltip.js"
 import initDropdownMenu from "./modules/dropdown-menu.js"
 import initMenuMobile from "./modules/menu-mobile.js"
 import initFuncionamento from "./modules/funcionamento.js"
@@ -22,8 +22,10 @@ tabNav.init()
 const modal = new Modal("[data-modal='abrir']", "[data-modal='fechar']", "[data-modal='container']")
 modal.init()
 
+const tooltip = new ToolTip('[data-tooltip]')
+tooltip.init()
+
 initAnimacaoScroll()
-initToolTip()
 initDropdownMenu()
 initMenuMobile()
 initFuncionamento()
